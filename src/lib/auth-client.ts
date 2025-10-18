b7fb7f29c86e5487deb25bb90563f4a6456b1502
mugid <bekslambek22@gmail.com>
2025-10-19 01:07:37 +0500

    fix: fix env variable typo

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -5,7 +5,7 @@ import { auth } from "./auth";
 
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
   plugins: [inferAdditionalFields<typeof auth>()],
 });
 
